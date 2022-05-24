4923b4e147f3f8dd452158b170aa7a6093b41c46
henricus_ade <adenanthera210@gmail.com>
2022-05-24 12:37:30 +0700

    first

--- 123200101_responsi/src/main/Main.java
+++ 123200101_responsi/src/main/Main.java
@@ -3,15 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package login_register;
+package main;
 
 /**
  *
  * @author user
  */
 public class Main {
-    public static void main(String[] args) {
-        Connector connect = new Connector();
-        View tampilan = new View();
+    public static void main(String[] args){
+    ViewMovie vm = new ViewMovie();
+    ModelMovie mm = new ModelMovie();
+    ControllerMovie cm = new ControllerMovie(mm, vm);
     }
 }
